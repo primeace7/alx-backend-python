ca730ae4a76d22f804a11e7963b8089acbbc1a97
Osaruonamen Jeffery O <osaruonamenjeffery@gmail.com>
2024-03-19 23:47:16 +0000

    Fix file annotation error in task 4 file

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,11 +3,11 @@
 
 
 import asyncio
-from typing import List
+from typing import Iterable
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List[asyncio.Task]:
+async def task_wait_n(n: int, max_delay: int) -> Iterable[asyncio.Task]:
     result = []
     for i in range(n):
         output = await task_wait_random(max_delay)
