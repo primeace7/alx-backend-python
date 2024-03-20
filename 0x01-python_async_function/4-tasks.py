51ef40c78c470bb0790d9a96babf37b0ea5f4f02
Osaruonamen Jeffery O <osaruonamenjeffery@gmail.com>
2024-03-20 06:48:57 +0000

    Fix task 2 code error

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,7 +7,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List:
+async def task_wait_n(n: int, max_delay: int) -> List[asyncio.Task]:
     result = []
     for i in range(n):
         output = await task_wait_random(max_delay)
